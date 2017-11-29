782165cb4ca301b1656349103e396c86b93c3c80
gabbyyou <qianhuiy@marshall.usc.edu>
2017-11-29 02:09:33 -0800

    Add files via upload

---
+++ barChart/organs-config.js
@@ -0,0 +1,380 @@
+﻿var organs_config = {
+	'organ_1':{
+		'hover': '<b><u>APPENDIX</u></b><br>',//organ hover content
+		'enable':true,//true/false to enable/disable this organ
+		'url':'http://codecanyon.net/user/art101/portfolio',//Goto URL
+		'target':'same_window',//open link in new window:new_window, open in current window:same_window, or none for nothing.
+		
+		'upColor':'red',//organ color when page loads
+		'upOpacity':'0',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+		'outlineUpColor':'#0000FF',//outline color when page loads
+		'outlineUpOpacity':'0',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+
+		'overColor':'red',//organ color when mouse hover
+		'overOpacity':'0.3',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+		'outlineOverColor':'red',//outline color when mouse hover
+		'outlineOverOpacity':'1',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+		
+		'downColor':'#993366',//organ color when mouse clicking
+		'downOpacity':'0.5',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+		'outlineDownColor':'#FFFF00',//outline color when mouse clicking
+		'outlineDownOpacity':'0.8',// opacity of that color from 0-1 (e.g 0.3, can be 0 to hide it)
+	},
+	'organ_2':{
+		'hover': '<b><u>BLADDER</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_3':{
+		'hover': '<b><u>DIAPHRAGM</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_4':{
+		'hover': '<b><u>GALLBLADDER</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_5':{
+		'hover': '<b><u>HEART</u></b><br><span style="color: #E55451">SO2, CO and Ozone can cause severe heart diseases.</span><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'grey',
+		'overOpacity':'0.7',
+		'outlineOverColor':'grey',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_6':{
+		'hover': '<b><u>KIDNEYS</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_7':{
+		'hover': '<b><u>LARGE INTESTINE</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_8':{
+		'hover': '<b><u>LARYNX</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_9':{
+		'hover': '<b><u>LIVER</u></b><br><span style="color: #E55451">NO2 can cause severe liver diseases.</span><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'grey',
+		'overOpacity':'0.7',
+		'outlineOverColor':'grey',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_10':{
+		'hover': '<b><u>LUNGS</u></b><br><span style="color: #E55451">CO, NO2, Ozone and SO2 can cause severe lung diseases.</span><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'grey',
+		'overOpacity':'0.7',
+		'outlineOverColor':'grey',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_11':{
+		'hover': '<b><u>MAJOR BLOOD VESSELS</u></b><br><span style="color: #E55451">CO, NO2 and Ozone can cause severe problems to vessels. </span><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'grey',
+		'overOpacity':'0.7',
+		'outlineOverColor':'grey',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_12':{
+		'hover': '<b><u>PANCREAS</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_13':{
+		'hover': '<b><u>SMALL INTESTINE</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_14':{
+		'hover': '<b><u>SPLEEN</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_15':{
+		'hover': '<b><u>STOMACH</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_16':{
+		'hover': '<b><u>THYROID</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_17':{
+		'hover': '<b><u>TRACHEA</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	},
+	'organ_18':{
+		'hover': '<b><u>URETERS</u></b><br>',
+		'enable':true,
+		'url':'http://codecanyon.net/user/art101/portfolio',
+		'target':'same_window',
+		
+		'upColor':'#FF0000',
+		'upOpacity':'0',
+		'outlineUpColor':'#0000FF',
+		'outlineUpOpacity':'0',
+
+		'overColor':'#FF0000',
+		'overOpacity':'0.3',
+		'outlineOverColor':'#FF0000',
+		'outlineOverOpacity':'1',
+		
+		'downColor':'#993366',
+		'downOpacity':'0.5',
+		'outlineDownColor':'#FFFF00',
+		'outlineDownOpacity':'0.8',
+	}
+}
---
+++ barChart/organs-interact.js
@@ -0,0 +1,158 @@
+﻿// Quick feature detection
+function isTouchEnabled() {
+	return (('ontouchstart' in window)
+		|| (navigator.MaxTouchPoints > 0)
+		|| (navigator.msMaxTouchPoints > 0));
+}
+
+$(function(){
+	addEvent('organ_1');
+	addEvent('organ_2');
+	addEvent('organ_3');
+	addEvent('organ_4');
+	addEvent('organ_5');
+	addEvent('organ_6');
+	addEvent('organ_7');
+	addEvent('organ_8');
+	addEvent('organ_9');
+	addEvent('organ_10');
+	addEvent('organ_11');
+	addEvent('organ_12');
+	addEvent('organ_13');
+	addEvent('organ_14');
+	addEvent('organ_15');
+	addEvent('organ_16');
+	addEvent('organ_17');
+	addEvent('organ_18');
+})
+
+
+function addEvent(id,relationId){
+	var _obj = $('#'+id);
+	_obj.attr({'fill':organs_config[id]['upColor'],'fill-opacity':organs_config[id]['upOpacity'],'stroke':organs_config[id]['outlineUpColor'],'stroke-opacity':organs_config[id]['outlineUpOpacity']});
+	_obj.attr({'cursor':'default'});
+	if(organs_config[id]['enable'] == true){
+		if (isTouchEnabled()) {
+			_obj.on('touchstart', function(e){
+				var touch = e.originalEvent.touches[0];
+				var x=touch.pageX+10, y=touch.pageY+15;
+				var tipw=$('#organs-tip').outerWidth(), tiph=$('#organs-tip').outerHeight(), 
+				x=(x+tipw>$(document).scrollLeft()+$(window).width())? x-tipw-(20*2) : x
+				y=(y+tiph>$(document).scrollTop()+$(window).height())? $(document).scrollTop()+$(window).height()-tiph-10 : y
+				$('#'+id).css({'fill':organs_config[id]['overColor'],'fill-opacity':organs_config[id]['downOpacity'],'stroke':organs_config[id]['outlineDownColor'],'stroke-opacity':organs_config[id]['outlineDownOpacity']});
+				$('#organs-tip').show().html(organs_config[id]['hover']);
+				$('#organs-tip').css({left:x, top:y})
+			})
+			_obj.on('touchend', function(){
+				$('#'+id).css({'fill':organs_config[id]['upColor'],'fill-opacity':organs_config[id]['upOpacity'],'stroke':organs_config[id]['outlineUpColor'],'stroke-opacity':organs_config[id]['outlineUpOpacity']});
+				if(organs_config[id]['target'] == 'new_window'){
+					window.open(organs_config[id]['url']);	
+				}else if(organs_config[id]['target'] == 'same_window'){
+					window.parent.location.href=organs_config[id]['url'];
+				}
+			})
+		}
+		_obj.attr({'cursor':'pointer'});
+		_obj.hover(function(){
+			$('#organs-tip').show().html(organs_config[id]['hover']);
+			_obj.css({'fill':organs_config[id]['overColor'],'fill-opacity':organs_config[id]['overOpacity'],'stroke':organs_config[id]['outlineOverColor'],'stroke-opacity':organs_config[id]['outlineOverOpacity']})
+		},function(){
+			$('#organs-tip').hide();
+			$('#'+id).css({'fill':organs_config[id]['upColor'],'fill-opacity':organs_config[id]['upOpacity'],'stroke':organs_config[id]['outlineUpColor'],'stroke-opacity':organs_config[id]['outlineUpOpacity']});
+		})
+		_obj.mousedown(function(){
+			$('#'+id).css({'fill':organs_config[id]['downColor'],'fill-opacity':organs_config[id]['downOpacity'],'stroke':organs_config[id]['outlineDownColor'],'stroke-opacity':organs_config[id]['outlineDownOpacity']});
+		})
+		_obj.mouseup(function(){
+			$('#'+id).css({'fill':organs_config[id]['overColor'],'fill-opacity':organs_config[id]['overOpacity'],'stroke':organs_config[id]['outlineOverColor'],'stroke-opacity':organs_config[id]['outlineOverOpacity']});
+			if(organs_config[id]['target'] == 'new_window'){
+				window.open(organs_config[id]['url']);	
+			}else if(organs_config[id]['target'] == 'same_window'){
+				window.parent.location.href=organs_config[id]['url'];
+			}
+		})
+		_obj.mousemove(function(e){
+			var x=e.pageX+10, y=e.pageY+15;
+			var tipw=$('#organs-tip').outerWidth(), tiph=$('#organs-tip').outerHeight(), 
+			x=(x+tipw>$(document).scrollLeft()+$(window).width())? x-tipw-(20*2) : x
+			y=(y+tiph>$(document).scrollTop()+$(window).height())? $(document).scrollTop()+$(window).height()-tiph-10 : y
+			$('#organs-tip').css({left:x, top:y})
+		})
+	}	
+}
+
+//The hotspots code
+/*$(function(){
+	var points_len = hotspots_config['hotspots'].length;
+	if( points_len > 0){
+		var xmlns = "http://www.w3.org/2000/svg";
+		var tsvg_obj = document.getElementById("organs_hotspots");
+		var svg_circle;
+		for(var i=0;i<points_len;i++){
+			svg_circle = document.createElementNS(xmlns, "circle");
+			svg_circle.setAttributeNS(null, "cx", hotspots_config['hotspots'][i]['pos_X']);
+			svg_circle.setAttributeNS(null, "cy", hotspots_config['hotspots'][i]['pos_Y']);
+			svg_circle.setAttributeNS(null, "r", hotspots_config['hotspots'][i]['diameter']/2);
+			svg_circle.setAttributeNS(null, "fill", hotspots_config['hotspots'][i]['upColor']);
+			svg_circle.setAttributeNS(null, "fill-opacity", hotspots_config['hotspots'][i]['upOpacity']);
+			svg_circle.setAttributeNS(null, "stroke",hotspots_config['hotspots'][i]['outlineUpColor']);
+			svg_circle.setAttributeNS(null, "stroke-opacity",hotspots_config['hotspots'][i]['outlineUpOpacity']);
+			svg_circle.setAttributeNS(null, "id",'organs_hotspots_'+i);
+			tsvg_obj.appendChild(svg_circle);
+			dynamicAddEvent(i);
+		}
+	}
+});
+
+function dynamicAddEvent(id){
+	var obj = $('#organs_hotspots_'+id);
+
+	if(hotspots_config['hotspots'][id]['enable'] == true){
+		if (isTouchEnabled()) {
+			obj.on('touchstart', function(e){
+				var touch = e.originalEvent.touches[0];
+				var x=touch.pageX+10, y=touch.pageY+15;
+				var tipw=$('#organs-tip').outerWidth(), tiph=$('#organs-tip').outerHeight(),
+				x=(x+tipw>$(document).scrollLeft()+$(window).width())? x-tipw-(20*2) : x
+				y=(y+tiph>$(document).scrollTop()+$(window).height())? $(document).scrollTop()+$(window).height()-tiph-10 : y
+				$('#'+id).css({'fill':hotspots_config['hotspots'][id]['downColor'],'fill-opacity':hotspots_config['hotspots'][id]['downOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineDownColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineDownOpacity']});
+				$('#organs-tip').show().html(hotspots_config['hotspots'][id]['hover']);
+				$('#organs-tip').css({left:x, top:y})
+			})
+			obj.on('touchend', function(){
+				$('#'+id).css({'fill':hotspots_config['hotspots'][id]['upColor'],'fill-opacity':hotspots_config['hotspots'][id]['upOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineUpColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineUpOpacity']});
+				if(hotspots_config['hotspots'][id]['target'] == 'new_window'){
+					window.open(hotspots_config['hotspots'][id]['url']);
+				}else if(hotspots_config['hotspots'][id]['target'] == 'same_window'){
+					window.parent.location.href=hotspots_config['hotspots'][id]['url'];
+				}
+			})
+		}
+		obj.attr({'cursor':'pointer'});
+		obj.hover(function(){
+			$('#organs-tip').show().html(hotspots_config['hotspots'][id]['hover']);
+			obj.css({'fill':hotspots_config['hotspots'][id]['overColor'],'fill-opacity':hotspots_config['hotspots'][id]['overOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineOverColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineOverOpacity']})
+		},function(){
+			$('#organs-tip').hide();
+			obj.css({'fill':hotspots_config['hotspots'][id]['upColor'],'fill-opacity':hotspots_config['hotspots'][id]['upOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineUpColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineUpOpacity']});
+		})
+		obj.mousedown(function(){
+			obj.css({'fill':hotspots_config['hotspots'][id]['downColor'],'fill-opacity':hotspots_config['hotspots'][id]['downOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineDownColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineDownOpacity']});
+		})
+		obj.mouseup(function(){
+			obj.css({'fill':hotspots_config['hotspots'][id]['overColor'],'fill-opacity':hotspots_config['hotspots'][id]['overOpacity'],'stroke':hotspots_config['hotspots'][id]['outlineOverColor'],'stroke-opacity':hotspots_config['hotspots'][id]['outlineOverOpacity']});
+			if(hotspots_config['hotspots'][id]['target'] == 'new_window'){
+				window.open(hotspots_config['hotspots'][id]['url']);	
+			}else if(hotspots_config['hotspots'][id]['target'] == 'same_window'){
+				window.parent.location.href=hotspots_config['hotspots'][id]['url'];
+			}
+		})
+		obj.mousemove(function(e){
+				var x=e.pageX+10, y=e.pageY+15;
+				var tipw=$('#organs-tip').outerWidth(), tiph=$('#organs-tip').outerHeight(), 
+				x=(x+tipw>$(document).scrollLeft()+$(window).width())? x-tipw-(20*2) : x
+				y=(y+tiph>$(document).scrollTop()+$(window).height())? $(document).scrollTop()+$(window).height()-tiph-10 : y
+				$('#organs-tip').css({left:x, top:y})
+		})
+	}	
+}*/
